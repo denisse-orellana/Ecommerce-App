fc9f47b5a40363081e9bc60a30f7c53b829bbab6
Denisse Orellana <denissep.orellana@gmail.com>
2022-10-31 13:10:43 -0300

    Initial state

---
+++ app.js
@@ -0,0 +1,165 @@
+import { productos } from "./products.js"
+
+let elMostrarBtn = document.getElementById('mostrarBtn')
+let mercadoLibreButton = document.getElementById('mercadoLibreButton')
+let url = "https://api.mercadolibre.com/sites/MLA/search"
+
+const isOk = true
+
+const customFetch = (time, task) => {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          if (isOk) {
+              resolve(task)
+          } else {
+              reject("Error")
+          }
+      }, time)
+  })
+}
+
+
+// ---------------------------  Then Promise ---------------------------- //
+// const mostrarProductos = () => {
+//   customFetch(2000, productos).then(
+//       data => {
+//           let lista = document.createElement("div")
+//           for (let i=0; i<data.length; i++) {
+//               let h3 = document.createElement("h3")
+//               h3.innerHTML = data[i].title
+
+//               let cardDiv = document.createElement("div")
+//               cardDiv.className = "card-product"
+
+//               let image = document.createElement("img")
+//               image.src = data[i].image
+
+//               let price = document.createElement("p")
+//               price.innerHTML = `US$ ${data[i].price}`
+
+//               let buyButton = document.createElement("button")
+//               buyButton.innerHTML = 'Buy'
+
+//               cardDiv.append(h3)
+//               cardDiv.append(image)
+//               cardDiv.append(price)
+//               cardDiv.append(buyButton)
+//               lista.append(cardDiv)
+//           }
+//           document.getElementById("root").append(lista)
+//       }
+//   )
+// }
+// ---------------------------  Then Promise ---------------------------- //
+
+
+
+// ---------------------------  Async Await Promise --------------------- //
+const mostrarProductos = async() => {
+    try {
+        const data = await customFetch(2000, productos)
+        // console.log(data)
+        getCard(data)
+        return data
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const getCard = (data) => {
+    let lista = document.createElement("div")
+    for (let i=0; i<data.length; i++) {
+        let h3 = document.createElement("h3")
+        h3.innerHTML = data[i].title
+
+        let cardDiv = document.createElement("div")
+        cardDiv.className = "card-product"
+
+        let image = document.createElement("img")
+        image.src = data[i].image
+
+        let price = document.createElement("p")
+        price.innerHTML = `US$ ${data[i].price}`
+
+        let buyButton = document.createElement("button")
+        buyButton.innerHTML = 'Buy'
+
+        cardDiv.append(h3)
+        cardDiv.append(image)
+        cardDiv.append(price)
+        cardDiv.append(buyButton)
+        lista.append(cardDiv)   
+    }
+    document.getElementById("root").append(lista)
+
+    elMostrarBtn.style.display = "none"
+    mercadoLibreButton.style.display = "none"
+    backBtn.style.display = "inline"
+}
+// ---------------------------  Async Await Promise --------------------- //
+
+
+const mostrarProductosML = async() => {
+    try {
+        setTimeout(async () => {
+            const response = await axios.get(url, {
+                params: {
+                    q: 'libros'
+                },
+            })
+
+            if (response.status === 200) {
+                const data = response.data
+                console.log(data)
+                addCard(data)
+                return data
+            } else if (response.status = 404) {
+                console.log('The product cannot be found.')
+            } else {
+                console.log('Ups.. Something went wrong.')
+            }
+
+          }, 2000)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const addCard = (data) => {
+    let products = data.results
+    // console.log(products)
+    products.forEach(product => {
+        let div = document.createElement("div")
+        div.className = "card-product"
+
+        let h3 = document.createElement("h3")
+        h3.innerHTML = product.title
+
+        let image = document.createElement("img")
+        image.src = product.thumbnail
+        image.alt = product.title
+
+        let price = document.createElement("p")
+        price.innerHTML = `${product.prices.prices[0].currency_id} $${product.prices.prices[0].amount}`
+
+        let buyButton = document.createElement("a")
+        buyButton.style.display = "inline"
+        buyButton.innerHTML = 'Buy'
+        buyButton.href = product.permalink
+
+        div.append(h3)
+        div.append(image)
+        div.append(price)
+        div.append(buyButton)
+        document.getElementById("root").append(div)
+
+        elMostrarBtn.style.display = "none"
+        mercadoLibreButton.style.display = "none"
+        backBtn.style.display = "inline"
+        backBtn.style.background = "#FFCC70"
+        backBtn.style.color = "#282828"
+    });
+}
+
+elMostrarBtn.addEventListener('click', mostrarProductos)
+mercadoLibreButton.addEventListener('click', mostrarProductosML)
---
+++ products.js
@@ -0,0 +1,72 @@
+export const productos = [
+  {
+    id: 1,
+    title: 'Tablet Almost New Last Model',
+    price: 85,
+    stock: 70,
+    image: 'https://home.ripley.cl/store/Attachment/WOP/D113/2000379185788/2000379185788_2.jpg'
+  },
+  {
+    id: 2,
+    title: 'Smartphone X 22 Model',
+    price: 99,
+    stock: 50,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGpx5nmcY_IYSSfdG56PqXmZEuB51ozQnlilnMhExHol75jte47pe444eLHc_L8k6wSsI&usqp=CAU'
+  }, 
+  {
+    id: 3,
+    title: 'Laptop NewModel 22',
+    price: 99,
+    stock: 200,
+    image: 'https://media.wired.com/photos/5fb2cc575c9914713ead03de/master/w_1920,c_limit/Gear-Apple-MacBook-Air-top-down-SOURCE-Apple.jpg'
+  }, 
+  {
+    id: 4,
+    title: 'Mouse NewModel',
+    price: 15,
+    stock: 600,
+    image: 'https://image.shutterstock.com/image-photo/wireless-computer-mouse-isolated-on-260nw-95568295.jpg'
+  }, 
+  {
+    id: 5,
+    title: 'Printer NewModel 22',
+    price: 85,
+    stock: 45,
+    image: 'https://i.pcmag.com/imagery/roundups/00DKaTkOIH7Z34XdzIgHtH9-8..v1569492690.jpg'
+  }, 
+  {
+    id: 6,
+    title: 'Monitor New Model',
+    price: 55,
+    stock: 20,
+    image: 'https://m.media-amazon.com/images/I/71aAsI1QIkL._SX679_.jpg'
+  }, 
+  {
+    id: 7,
+    title: 'Audifonos Air Pod 22',
+    price: 55,
+    stock: 20,
+    image: 'https://www.paris.cl/dw/image/v2/BCHW_PRD/on/demandware.static/-/Sites-cencosud-master-catalog/default/dw0ac91eb0/images/imagenes-productos/742/604974-0000-001.jpg?sw=320&sh=409&sm=fit'
+  }, 
+  {
+    id: 8,
+    title: 'Working Desk',
+    price: 199,
+    stock: 10,
+    image: 'https://nypost.com/wp-content/uploads/sites/2/2021/02/Coleshome-Desk.jpg'
+  }, 
+  {
+    id: 9,
+    title: 'Working Chair',
+    price: 35,
+    stock: 40,
+    image: 'https://m.media-amazon.com/images/I/61hUIz8wuyL.jpg'
+  }, 
+  {
+    id: 10,
+    title: 'Soporte computador',
+    price: 30,
+    stock: 50,
+    image: 'https://http2.mlstatic.com/D_NQ_NP_2X_837986-MLC40695367153_022020-F.webp'
+  }
+]
