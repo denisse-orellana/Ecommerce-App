e8194e4faf9e900f018e7692d63fc64b441fddac
Denisse Orellana <denissep.orellana@gmail.com>
2022-10-31 13:23:19 -0300

    change: app.js

--- app.js
+++ app.js
@@ -18,43 +18,6 @@ const customFetch = (time, task) => {
   })
 }
 
-
-// ---------------------------  Then Promise ---------------------------- //
-// const mostrarProductos = () => {
-//   customFetch(2000, productos).then(
-//       data => {
-//           let lista = document.createElement("div")
-//           for (let i=0; i<data.length; i++) {
-//               let h3 = document.createElement("h3")
-//               h3.innerHTML = data[i].title
-
-//               let cardDiv = document.createElement("div")
-//               cardDiv.className = "card-product"
-
-//               let image = document.createElement("img")
-//               image.src = data[i].image
-
-//               let price = document.createElement("p")
-//               price.innerHTML = `US$ ${data[i].price}`
-
-//               let buyButton = document.createElement("button")
-//               buyButton.innerHTML = 'Buy'
-
-//               cardDiv.append(h3)
-//               cardDiv.append(image)
-//               cardDiv.append(price)
-//               cardDiv.append(buyButton)
-//               lista.append(cardDiv)
-//           }
-//           document.getElementById("root").append(lista)
-//       }
-//   )
-// }
-// ---------------------------  Then Promise ---------------------------- //
-
-
-
-// ---------------------------  Async Await Promise --------------------- //
 const mostrarProductos = async() => {
     try {
         const data = await customFetch(2000, productos)
@@ -96,8 +59,6 @@ const getCard = (data) => {
     mercadoLibreButton.style.display = "none"
     backBtn.style.display = "inline"
 }
-// ---------------------------  Async Await Promise --------------------- //
-
 
 const mostrarProductosML = async() => {
     try {
