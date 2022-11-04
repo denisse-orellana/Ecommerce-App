0edb904f25bf09a690f83a2a116e347ca6ba9d06
Denisse Orellana <denissep.orellana@gmail.com>
2022-11-03 22:55:40 -0300

    change: app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { productos } from "./products.js"
 
-let elMostrarBtn = document.getElementById('mostrarBtn')
-let mercadoLibreButton = document.getElementById('mercadoLibreButton')
 let url = "https://api.mercadolibre.com/sites/MLA/search"
+
+let allProducts = []
 
 const isOk = true
 
@@ -22,105 +22,101 @@ const mostrarProductos = async() => {
     try {
         const data = await customFetch(2000, productos)
         // console.log(data)
-        getCard(data)
         return data
     } catch (error) {
         console.error(error);
     }
 }
 
-const getCard = (data) => {
-    let lista = document.createElement("div")
-    for (let i=0; i<data.length; i++) {
-        let h3 = document.createElement("h3")
-        h3.innerHTML = data[i].title
-
-        let cardDiv = document.createElement("div")
-        cardDiv.className = "card-product"
-
-        let image = document.createElement("img")
-        image.src = data[i].image
-
-        let price = document.createElement("p")
-        price.innerHTML = `US$ ${data[i].price}`
-
-        let buyButton = document.createElement("button")
-        buyButton.innerHTML = 'Buy'
-
-        cardDiv.append(h3)
-        cardDiv.append(image)
-        cardDiv.append(price)
-        cardDiv.append(buyButton)
-        lista.append(cardDiv)   
-    }
-    document.getElementById("root").append(lista)
-
-    elMostrarBtn.style.display = "none"
-    mercadoLibreButton.style.display = "none"
-    backBtn.style.display = "inline"
-}
-
 const mostrarProductosML = async() => {
     try {
-        setTimeout(async () => {
-            const response = await axios.get(url, {
-                params: {
-                    q: 'libros'
-                },
-            })
-
-            if (response.status === 200) {
-                const data = response.data
-                console.log(data)
-                addCard(data)
-                return data
-            } else if (response.status = 404) {
-                console.log('The product cannot be found.')
-            } else {
-                console.log('Ups.. Something went wrong.')
-            }
-
-          }, 2000)
+        const response = await axios.get(url, {
+            params: {
+                q: 'tecnología'
+            },
+        })
+        if (response.status === 200) {
+            const data = response.data
+            // console.log(data)
+            return data
+        } else if (response.status = 404) {
+            console.log('The product cannot be found.')
+        } else {
+            console.log('Ups.. Something went wrong.')
+        }
     } catch (error) {
         console.error(error)
     }
 }
 
-const addCard = (data) => {
-    let products = data.results
-    // console.log(products)
-    products.forEach(product => {
-        let div = document.createElement("div")
-        div.className = "card-product"
+const addCardProducts = (product) => {
+    let lista = document.createElement("div")
 
-        let h3 = document.createElement("h3")
-        h3.innerHTML = product.title
+    let h3 = document.createElement("h3")
+    h3.innerHTML = product.title
 
-        let image = document.createElement("img")
-        image.src = product.thumbnail
-        image.alt = product.title
+    let cardDiv = document.createElement("div")
+    cardDiv.className = "card-product"
 
-        let price = document.createElement("p")
-        price.innerHTML = `${product.prices.prices[0].currency_id} $${product.prices.prices[0].amount}`
+    let image = document.createElement("img")
+    image.src = product.image
 
-        let buyButton = document.createElement("a")
-        buyButton.style.display = "inline"
-        buyButton.innerHTML = 'Buy'
-        buyButton.href = product.permalink
+    let price = document.createElement("p")
+    price.innerHTML = `US$ ${product.price}`
 
-        div.append(h3)
-        div.append(image)
-        div.append(price)
-        div.append(buyButton)
-        document.getElementById("root").append(div)
+    let buyButton = document.createElement("button")
+    buyButton.innerHTML = 'Buy'
 
-        elMostrarBtn.style.display = "none"
-        mercadoLibreButton.style.display = "none"
-        backBtn.style.display = "inline"
-        backBtn.style.background = "#FFCC70"
-        backBtn.style.color = "#282828"
-    });
+    cardDiv.append(h3)
+    cardDiv.append(image)
+    cardDiv.append(price)
+    cardDiv.append(buyButton)
+    lista.append(cardDiv)   
+
+    document.getElementById("root").append(lista)
 }
 
-elMostrarBtn.addEventListener('click', mostrarProductos)
-mercadoLibreButton.addEventListener('click', mostrarProductosML)
+const addCardProductsML = (product) => {
+    let div = document.createElement("div")
+    div.className = "card-product"
+
+    let h3 = document.createElement("h3")
+    h3.innerHTML = product.title
+
+    let image = document.createElement("img")
+    image.src = product.thumbnail
+    image.alt = product.title
+
+    let price = document.createElement("p")
+    price.innerHTML = `${product.prices.prices[0].currency_id} $${product.prices.prices[0].amount}`
+
+    let buyButton = document.createElement("a")
+    buyButton.style.display = "inline"
+    buyButton.innerHTML = 'Buy'
+    buyButton.href = product.permalink
+
+    div.append(h3)
+    div.append(image)
+    div.append(price)
+    div.append(buyButton)
+    document.getElementById("root").append(div)
+}
+
+const addCard = (products) => {
+    products.forEach(product => {
+        (typeof product.id === 'number') ? addCardProducts(product) : addCardProductsML(product)
+    })
+}
+
+const showProducts = async () => {
+    let productsOne = await mostrarProductos()
+
+    let productsML = await mostrarProductosML()
+    let productsTwo = productsML.results
+
+    allProducts = [ ...productsOne, ...productsTwo]
+    console.log(allProducts)
+    addCard(allProducts)
+}
+
+window.addEventListener('load', showProducts)
