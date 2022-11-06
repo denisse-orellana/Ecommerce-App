5e278afd175c1678561267e6f64ed43bffc1473e
Denisse Orellana <denissep.orellana@gmail.com>
2022-11-06 16:43:11 -0300

    add: delete product from cart

--- app.js
+++ app.js
@@ -1,4 +1,76 @@
-import { productos } from "./products.js"
+const productos = [
+    {
+      id: 1,
+      title: 'Tablet Almost New Last Model',
+      price: 85,
+      stock: 70,
+      image: 'https://home.ripley.cl/store/Attachment/WOP/D113/2000379185788/2000379185788_2.jpg'
+    },
+    {
+      id: 2,
+      title: 'Smartphone X 22 Model',
+      price: 99,
+      stock: 50,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGpx5nmcY_IYSSfdG56PqXmZEuB51ozQnlilnMhExHol75jte47pe444eLHc_L8k6wSsI&usqp=CAU'
+    }, 
+    {
+      id: 3,
+      title: 'Laptop NewModel 22',
+      price: 99,
+      stock: 200,
+      image: 'https://media.wired.com/photos/5fb2cc575c9914713ead03de/master/w_1920,c_limit/Gear-Apple-MacBook-Air-top-down-SOURCE-Apple.jpg'
+    }, 
+    {
+      id: 4,
+      title: 'Mouse NewModel',
+      price: 15,
+      stock: 600,
+      image: 'https://image.shutterstock.com/image-photo/wireless-computer-mouse-isolated-on-260nw-95568295.jpg'
+    }, 
+    {
+      id: 5,
+      title: 'Printer NewModel 22',
+      price: 85,
+      stock: 45,
+      image: 'https://i.pcmag.com/imagery/roundups/00DKaTkOIH7Z34XdzIgHtH9-8..v1569492690.jpg'
+    }, 
+    {
+      id: 6,
+      title: 'Monitor New Model',
+      price: 55,
+      stock: 20,
+      image: 'https://m.media-amazon.com/images/I/71aAsI1QIkL._SX679_.jpg'
+    }, 
+    {
+      id: 7,
+      title: 'Audifonos Air Pod 22',
+      price: 55,
+      stock: 20,
+      image: 'https://www.paris.cl/dw/image/v2/BCHW_PRD/on/demandware.static/-/Sites-cencosud-master-catalog/default/dw0ac91eb0/images/imagenes-productos/742/604974-0000-001.jpg?sw=320&sh=409&sm=fit'
+    }, 
+    {
+      id: 8,
+      title: 'Working Desk',
+      price: 199,
+      stock: 10,
+      image: 'https://nypost.com/wp-content/uploads/sites/2/2021/02/Coleshome-Desk.jpg'
+    }, 
+    {
+      id: 9,
+      title: 'Working Chair',
+      price: 35,
+      stock: 40,
+      image: 'https://m.media-amazon.com/images/I/61hUIz8wuyL.jpg'
+    }, 
+    {
+      id: 10,
+      title: 'Soporte computador',
+      price: 30,
+      stock: 50,
+      image: 'https://http2.mlstatic.com/D_NQ_NP_2X_837986-MLC40695367153_022020-F.webp'
+    }
+  ]
+// import { productos } from "./products.js"
 
 let url = "https://api.mercadolibre.com/sites/MLA/search"
 
@@ -137,13 +209,19 @@ const showProducts = async () => {
     addCard(allProducts)
 }
 
-window.addEventListener('DOMContentLoaded', showProducts)
-
 // Adding the states
 
 // 1. State
 const cartState = {
     cart: []
+}
+
+//  7. Remove product from the cart
+let deleteProduct = (indexProduct) => {
+    const prevCartState = getCartState()
+    actualCartState = prevCartState.cart.filter((product, index) => index != parseInt(indexProduct))
+    console.log(actualCartState)
+    setCartState({ cart: actualCartState })
 }
 
 // 2. Template UI
@@ -152,7 +230,7 @@ const cartTemplate = () => {
         return `<p><i class="fa-regular fa-circle-xmark"></i> Empty</p>`
     }
 
-    let cartProducts = cartState.cart.map((product, index) => `<li>${product.title} <button onclick="removeProduct(${index})"><i class="fa-solid fa-xmark"></i></button>`).join("")
+    let cartProducts = cartState.cart.map((product, indexProduct) => `<li>${product.title} <button onclick="deleteProduct(${indexProduct})" class="remove-product-button"><i class="fa-solid fa-xmark"></i></button></li>`).join("")
     return cartProducts
 }
 
@@ -162,8 +240,6 @@ const renderCart = () => {
     if (!cartList) return
     cartList.innerHTML = cartTemplate()
 }
-
-document.addEventListener('DOMContentLoaded', renderCart)
 
 // 4. Set State
 const setCartState = (obj) => {
@@ -179,15 +255,13 @@ const setCartState = (obj) => {
 const getCartState = () => JSON.parse(JSON.stringify(cartState))
 
 // 6. Add product to cart
-const addToCart = (i) => {
-    let product = allProducts[i]
+const addToCart = (index) => {
+    let product = allProducts[index]
 
     const lastCartState = getCartState()
     lastCartState.cart.push(product)
     setCartState(lastCartState)
 }
 
-//  7. Remove product from the cart
-const removeProduct = (index) => {
-    console.log('Removing product from cart...')
-}
+document.addEventListener('DOMContentLoaded', showProducts)
+document.addEventListener('DOMContentLoaded', renderCart)
